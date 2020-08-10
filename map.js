cdb3d81782716158a01534849c726b0abfd0e66d
jennylynnramz <60010423+jennylynnramz@users.noreply.github.com>
2020-08-09 19:04:34 -0700

    Maps shows San Francisco Area by default

--- map.js
+++ map.js
@@ -1,6 +1,5 @@
 
-var myMap = L.map('mapid').setView([51.505, -0.09], 13);
-
+var myMap = L.map('mapid').setView([37.78, -122.42], 13);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -9,4 +8,3 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: API_KEY
 }).addTo(myMap);
-
