d2ec2ce50018d9b0cee3e0e787b453fbdfb94ef5
Jennifer Ramsey <60010423+jennylynnramz@users.noreply.github.com>
2020-09-12 16:22:39 -0700

    updating to exclude violation explanations until a connection between datasets can be determined

--- LasVegas/riskLV.js
+++ LasVegas/riskLV.js
@@ -7,29 +7,33 @@ var restaurantInspectionsURL = "https://opendata.arcgis.com/datasets/94fe16f2a6a
 d3.json(restaurantInspectionsURL).then(function(restaurantInspectionsData) {
     var LVRestaurants = restaurantInspectionsData.features
     console.log("Restaurant Data")
-    console.log(LVRestaurants)
-    // load violation explanation data
-    d3.json(violationCodesURL).then(function(violationCodesData) {
-        var violationCodes = violationCodesData.features
-        console.log("Violation Data")
-        console.log(violationCodes)
+    // console.log(LVRestaurants)
 
-        violationRational = {}
+        LVRestaurantMinData = {}
+        for (i=0; i < LVRestaurants.length; i++){
+            // console.log(LVRestaurants[i])
+            LVRestaurantName = LVRestaurants[i].properties.Restaurant_Name
+            LVRestaurantAddress = LVRestaurants[i].properties.Address + ", " + LVRestaurants[i].properties.City + ", " + LVRestaurants[i].properties.State + ", " + LVRestaurants[i].properties.Zip
 
-        for (i=0; i < violationCodes.length; i++){
-            var violationCode = violationCodes[i].properties.Violation_Code
-            var violationDescription = violationCodes[i].properties.Violation_Description
-            violationRational[violationCode] = violationDescription
+            // console.log(LVRestaurants[i].properties.Inspection_Result)
+            console.log(LVRestaurants[i].properties.Inspection_Grade)
+
+
+
+
+            // console.log(LVRestaurantName)
+            // console.log(LVRestaurantAddress)
+
+
+
+            LVRestaurantMinData[LVRestaurantName] = {"Name": LVRestaurantName, 
+                                                    "Address" : LVRestaurantAddress,
+                                                    "Category" : LVRestaurants[i].properties.Category_Name, 
+                                                }
+
         }
 
-        // console.log(violationRational)
-        // for (var value in violationRational){
-        //     console.log(value)
-        // }
-
-        for (const property in violationRational) {
-            console.log(`${property}: ${violationRational[property]}`);
-        }
+        console.log(LVRestaurantMinData)
 
 
 
@@ -40,5 +44,24 @@ d3.json(restaurantInspectionsURL).then(function(restaurantInspectionsData) {
 
 
     
-    }); //d3.json(violationCodesURL)
 }); //d3.json(restaurantInspectionsURL)
+
+
+
+// Current_Demerits: 0
+// Current_Grade: "A"
+// Date_Current: "2020/03/23 00:00:00"
+// Employee_ID: "EE7000804"
+// Inspection_Date: "2020/03/23 00:00:00"
+// Inspection_Demerits: 0
+// Inspection_Grade: "A"
+// Inspection_Result: "'A' Grade"
+// Inspection_Time: "2020/03/23 15:20:00"
+// Inspection_Type: "Routine Inspection"
+// Location_1: "(36.1588472,115.2075095)"
+// ObjectId: 1295877
+// Permit_Number: "PR0098643"
+// Permit_Status: ""
+// Record_Updated: "2020/03/23 16:00:02"
+// Serial_Number: "DAIHHHL40"
+// Violations: 2928
